51b8311fac250786bc050a6cd52a70ba99ab5922
zimsneexh <zimsneexh@gmail.com>
2023-01-15 21:08:11 +0100

    Move code from main branch repo

---
+++ README.md
@@ -0,0 +1,5 @@
+# A very simple python3 logger written in python3
+
+TODO:
+- Support file log 
+- Support custom callback registration
---
+++ src/test.py
@@ -0,0 +1,36 @@
+from log import blog
+
+print("Terminal color disabled:")
+blog.CONFIG_OPTIONS["NO_TERM"] = True
+
+blog.info("info-level")
+blog.warn("warn-level")
+blog.error("error-level")
+blog.debug("debug-level")
+blog.web_log("web-level")
+
+print("\n\nTerminal color enabled:")
+blog.CONFIG_OPTIONS["NO_TERM"] = False
+
+blog.info("info-level")
+blog.warn("warn-level")
+blog.error("error-level")
+blog.debug("debug-level")
+blog.web_log("web-level")
+
+print("\n\n Registering callback test()")
+
+def test(module, loglevel, logmessage):
+    print("CALLBACK to test:")
+    print(module)
+    print(loglevel)
+    print(logmessage)
+
+blog.register_output_provider(test)
+blog.warn("Test!")
+blog.unregister_output_provider(test)
+
+print("enabling debug log..")
+blog.enable_debug_level()
+blog.debug("debug-level")
+blog.disable_debug_level()
